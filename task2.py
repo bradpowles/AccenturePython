3943e05576786025149d5248f480611e8f0f1a66
Brad Powles <pwizz@hotmail.co.uk>
2021-07-07 09:29:59 +0100

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,9 @@
+""" 
+Peter's Pool Problem
+
+Peter needs to work out how many litres of water he needs to fill his pool this summer!
+We can assume 1 cubic metre is 1000 litres. And the dimensions for his pool are 20 metres in length, 10 metres width and 2 metres deep.
+
+Create a python program to help Peter order the correct amount of water.
+"""
+
