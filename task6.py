a7e99dcd0ac0d21347929faea8fd75b9eac9b8c6
Brad Powles <pwizz@hotmail.co.uk>
2021-07-07 09:31:10 +0100

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,13 @@
+""" 
+FizzBuzz
+
+   Make it say fizzbuzz on numbers that are divisible 
+   by 3 and 5
+   
+   Make it say fizz on numbers that are divisible by 3
+   
+   Make it say buzz on Buzz that are divisible by 5
+
+from a range of 100
+"""
+
