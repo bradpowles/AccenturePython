faf67b0c78dd16c7a30b605c0cd99f7f6cf77c08
Brad Powles <pwizz@hotmail.co.uk>
2021-07-07 09:30:13 +0100

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,20 @@
+"""
+Spot the bugs!
+
+There are 3 bugs (Syntax Errors) in the code below. Find and fix the bugs to get the program to work.
+
+Bonus points if you can tell what the program is for?
+"""
+
+a == 2
+for i in range(50)
+  b = 1
+  a += 1
+  c = True
+  while b < int(a**0.5):
+      b += 1
+      if a % b == 0:
+        c = False
+        break
+  if c:
+      print a
